f2f7f9e08868e5a65a68ea2851245d37ec3be40a
mkreive <mkreive@gmail.com>
2022-09-22 11:27:45 +0300

    basic settup done

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../index.scss';
+
+const Header = function () {
+    return <div className='header' />;
+};
+
+export default Header;
---
+++ src/components/Topics.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../index.scss';
+
+const Topics = function () {
+    return <div className='topic' />;
+};
+
+export default Topics;
