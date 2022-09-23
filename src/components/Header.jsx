079177e672ea4e9ee9f6c50fb95a0cd99d99740f
mkreive <mkreive@gmail.com>
2022-09-23 09:25:29 +0300

    header component aded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../index.scss';
 
 const Header = function () {
-    return <div className='header' />;
+    const linkStyle = {
+        textDecoration: 'none',
+    };
+    return (
+        <header className='header'>
+            <div className='header__logo'>
+                <img
+                    className='header__logo--icon'
+                    src='https://res.cloudinary.com/kreiva/image/upload/v1663828096/EasyBank/logo_aejzhq.svg'
+                    alt='logo'
+                />
+            </div>
+
+            <nav className='header__nav'>
+                <div className='nav'>
+                    <Link to='/home' style={linkStyle} className='nav__link'>
+                        Home
+                    </Link>
+                    <Link to='/about' style={linkStyle} className='nav__link'>
+                        About
+                    </Link>
+                    <Link to='/contact' style={linkStyle} className='nav__link'>
+                        Contact
+                    </Link>
+                    <Link to='/blog' style={linkStyle} className='nav__link'>
+                        Blog
+                    </Link>
+                    <Link to='/careers' style={linkStyle} className='nav__link'>
+                        Careers
+                    </Link>
+                </div>
+            </nav>
+
+            <div className='header__btns'>
+                <button className='btn btn--gradient'>Request Invite</button>
+                <button className='btn btn--gradient'>Login</button>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
