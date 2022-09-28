bb065b2d55b554dfdc0740d12e0a6b56b9599e4a
mkreive <mkreive@gmail.com>
2022-09-28 10:27:18 +0300

    home component in progress

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,6 @@ const Home = function () {
     return (
         <div className='container'>
             <div className='home'>
-                <div className='home__photo' />
                 <div className='home__text'>
                     <h1 className='heading--biggest'>Next generation digital banking</h1>
                     <div className='text'>
@@ -14,6 +13,7 @@ const Home = function () {
                     </div>
                     <button className='btn btn--gradient'>Request Invite</button>
                 </div>
+                <div className='home__photo' />
                 <div className='home__img'></div>
             </div>
         </div>
